b1b2a6663a60b1fc78330999b9f0c354b0a9e154
wasif <ahmedwasif036@gmail.com>
2024-08-07 00:32:50 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+//Assignment java_Script//
+
+//Alert//
+//Q no: 01//
+// alert("Assalam-O-Alaikum");
+
+//Q no:02//
+// alert("Hello! Welcome to our website.");
+
+//Q no:03//
+// alert("This is java \nscript");
+
+//Variable and Strings//
+// //Q no:01//
+// let username;
+// console.log(username);
+
+// //Q no:02//
+// let myName = "wasifahmed"
+// console.log(myName);
+
+// //Q no:03//
+// let message = "Hello world";
+// alert(message);
+
+// //Q no:04//
+// let name = "wasifahmed";
+// let age = "16";
+// let course = "Web Mobile and App Development";
+// alert(name);
+// alert(age);
+// alert(course);
+
+//Q no:05//
+// let food = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(food);
+
+//Q no:06//
+let email = "ahmedwasif036@gmail.com";
+alert("My Email Is " + email);
+
+//Q no:07//
+let book = "A Smarter Way To Learn JavaScript";
+alert(book);
+
+//Q no:08// ***
+
+//Q no:09//
+let style = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” ";
+alert(style);
+
+//Variable For Numeber//
+//Q no:01
+
+let age = "I am " + age  ;
+alert(age);
+
+//Qno:02//**
+
+//Qno:03//
+
